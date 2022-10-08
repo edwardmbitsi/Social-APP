84bc92eae672f35f9a07079176154b19acdcdb10
edwardmbitsi <mbitsiedward@gmail.com>
2022-10-08 21:24:16 +0300

    saturday nights

--- app/src/main/java/com/example/socialapp/DashboardActivity.java
+++ app/src/main/java/com/example/socialapp/DashboardActivity.java
@@ -15,7 +15,7 @@ import com.google.firebase.auth.FirebaseUser;
 public class DashboardActivity extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
-    FirebaseAuth firebaseUser;
+    FirebaseUser firebaseUser;
     String myuid;
     ActionBar actionBar;
     BottomNavigationView navigationView;
@@ -36,6 +36,57 @@ public class DashboardActivity extends AppCompatActivity {
         // time the fragment should be shown to the user
         // in this case it is home fragment
         HomeFragment fragment = new HomeFragment();
-        FragmentTransaction fragment
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.nav_home, fragment, "");
+        fragmentTransaction.commit();
     }
+
+    private final BottomNavigationView.OnNavigationItemSelectedListener selectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            switch (menuItem.getItemId()) {
+
+                case R.id.nav_home:
+                    actionBar.setTitle("Home");
+                    HomeFragment fragment = new HomeFragment();
+                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.nav_home, fragment, "");
+                    fragmentTransaction.commit();
+                    return true;
+
+                case R.id.nav_profile:
+                    actionBar.setTitle("Profile");
+                    ProfileFragment fragment1 = new ProfileFragment();
+                    FragmentTransaction fragmentTransaction1 = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction1.replace(R.id.nav_profile, fragment1);
+                    fragmentTransaction1.commit();
+                    return true;
+
+                case R.id.nav_users:
+                    actionBar.setTitle("Users");
+                    UsersFragment fragment2 = new UsersFragment();
+                    FragmentTransaction fragmentTransaction2 = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction2.replace(R.id.nav_users, fragment2, "");
+                    fragmentTransaction2.commit();
+                    return true;
+
+                case R.id.nav_chat:
+                    actionBar.setTitle("Chats");
+                    ChatListFragment listFragment = new ChatListFragment();
+                    FragmentTransaction fragmentTransaction3 = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction3.replace(R.id.nav_chat, listFragment, "");
+                    fragmentTransaction3.commit();
+                    return true;
+
+                case R.id.nav_addblogs:
+                    actionBar.setTitle("Add Blogs");
+                    AddBlogsFragment fragment4 = new AddBlogsFragment();
+                    FragmentTransaction fragmentTransaction4 = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction4.replace(R.id.nav_addblogs, fragment4, "");
+                    fragmentTransaction4.commit();
+                    return true;
+            }
+            return false;
+        }
+    };
 }
