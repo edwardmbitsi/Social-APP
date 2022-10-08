ead7b01e6807fc96aa4d4d044ebcbe79c4e3208f
edwardmbitsi <mbitsiedward@gmail.com>
2022-10-08 15:07:59 +0300

    saturday coding session

--- app/src/main/java/com/example/socialapp/DashboardActivity.java
+++ app/src/main/java/com/example/socialapp/DashboardActivity.java
@@ -1,4 +1,41 @@
 package com.example.socialapp;
 
-public class DashboardActivity {
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    FirebaseAuth firebaseUser;
+    String myuid;
+    ActionBar actionBar;
+    BottomNavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+        actionBar = getSupportActionBar();
+        actionBar.setTitle("Profile Activity");
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        navigationView = findViewById(R.id.navigation);
+        navigationView.setOnNavigationItemSelectedListener(selectedListener);
+        actionBar.setTitle("Home");
+
+        // When we open the application first
+        // time the fragment should be shown to the user
+        // in this case it is home fragment
+        HomeFragment fragment = new HomeFragment();
+        FragmentTransaction fragment
+    }
 }
